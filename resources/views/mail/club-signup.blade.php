006a98598f550e887f7e93e2a93fedb59e546ec5
marvin <ymarvintan@gmail.com>
2026-08-18 23:46:08 +0800

    add

--- resources/views/mail/club-signup.blade.php
+++ resources/views/mail/club-signup.blade.php
@@ -11,8 +11,6 @@ Someone signed up for the weekly community ride on the website.
 **Mobile:** {{ $riderPhone }}
 </x-mail::panel>
 
-Add them to the riders group chat, and collect the fee when they turn up.
-
 Replying to this email goes straight back to {{ $riderName }}.
 
 Thanks,<br>
